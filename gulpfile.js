4651a6d168c6eac7939ea4ede764552c66b5fddf
froke <froke@192.168.1.66>
2016-04-20 02:06:31 -0500

    Bower (Bootstrap & Font-Awesome)

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var gulp = require('gulp');
 var elixir = require('laravel-elixir');
 
 /*
@@ -11,6 +12,40 @@ var elixir = require('laravel-elixir');
  |
  */
 
+gulp.task("copyfiles", function() {
+
+  gulp.src("vendor/bower_components/jquery/dist/jquery.js")
+    .pipe(gulp.dest("resources/assets/js/"));
+
+  gulp.src("vendor/bower_components/bootstrap/less/**")
+    .pipe(gulp.dest("resources/assets/less/bootstrap"));
+
+  gulp.src("vendor/bower_components/bootstrap/dist/js/bootstrap.js")
+    .pipe(gulp.dest("resources/assets/js/"));
+
+  gulp.src("vendor/bower_components/bootstrap/dist/fonts/**")
+    .pipe(gulp.dest("public/assets/fonts"));
+   
+  gulp.src("vendor/bower_components/font-awesome/less/**")
+    .pipe(gulp.dest("resources/assets/less/font-awesome"));
+    
+  gulp.src("vendor/bower_components/font-awesome/fonts/**")
+    .pipe(gulp.dest("public/assets/fonts"));
+
+});
+
 elixir(function(mix) {
     mix.sass('app.scss');
+    
+    // Combine scripts
+    mix.scripts([
+        'js/jquery.js',
+        'js/bootstrap.js'
+    ],
+    'public/assets/js/app.js',
+    'resources/assets'
+    );
+
+    // Compile Less
+    mix.less('app.less', 'public/assets/css/app.css');
 });
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Classroom Finder</title>
+        <title>Salones Upiicsa</title>
         @include('globals.css')
         @yield('css')
     </head>
