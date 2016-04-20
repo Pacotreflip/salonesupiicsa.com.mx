f018f134dd06172fef6548ee9aba893c6689ac17
froke <froke@192.168.1.66>
2016-04-20 00:10:11 -0500

    Base de Datos, roles y permisos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use App\Role;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,9 +15,49 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
+        
+        Role::create([
+            'name' => 'admin',
+            'display_name' => 'Administrador',
+            'description' => 'Administrador del Sistema'            
+		]);
+        
+        Role::create([
+            'name' => 'academia',
+            'display_name' => 'Academia',
+            'description' => 'Administración de Academia'            
+		]);
+        
+        Role::create([
+            'name' => 'usuario',
+            'display_name' => 'Usuario',
+            'description' => 'Usuario del Sistema'            
+        ]);
+        
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'administrador@ipn.mx',
+            'password' => bcrypt('administrador')
+        ])->attachRole(1);
+        
+        User::create([
+            'name' => 'Computación',
+            'email' => 'computacion@ipn.mx',
+            'password' => bcrypt('computacion')
+        ])->attachRole(2);
 
-        // $this->call(UserTableSeeder::class);
-
+        User::create([
+            'name' => 'Finanzas',
+            'email' => 'finanzas@ipn.mx',
+            'password' => bcrypt('finanzas')
+        ])->attachRole(2);    
+        
+        User::create([
+            'name' => 'Francisco',
+            'email' => 'francisco@ipn.mx',
+            'password' => bcrypt('francisco')
+        ])->attachRole(3);    
+          
         Model::reguard();
     }
 }
